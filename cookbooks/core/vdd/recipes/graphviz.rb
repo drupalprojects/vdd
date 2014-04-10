de6cf2ab0e1ed5584f6b8cff084b50d6ed8a37e7
ericrdb <ericrdb@226785.no-reply.drupal.org>
2014-04-10 07:23:33 +0300

    Issue #2226599 by ericrdb: Install Webgrind web dashboard to view XDebug results.

---
+++ cookbooks/core/vdd/recipes/graphviz.rb
@@ -0,0 +1,3 @@
+package "graphviz" do
+  action :install
+end
---
+++ cookbooks/core/vdd/recipes/webgrind.rb
@@ -0,0 +1,9 @@
+git "/var/www/webgrind" do
+  repository "https://github.com/jokkedk/webgrind.git"
+  reference "master"
+  action :sync
+end
+
+link "/usr/local/bin/dot" do
+  to "/usr/bin/dot"
+end
