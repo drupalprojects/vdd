de302004da0367324399e0ceff17c873c6f2fd83
John Ennew <johne@deeson.co.uk>
2015-02-26 16:23:06 +0000

    Added a cookbook for solr

---
+++ chef/cookbooks/berks/solr/attributes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: solr
+# Attributes:: default
+#
+# Copyright 2013, David Radcliffe
+#
+
+default['solr']['version']  = '4.6.1'
+default['solr']['url']      = "https://archive.apache.org/dist/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
+default['solr']['data_dir'] = '/etc/solr'
+default['solr']['dir']      = '/opt/solr'
+default['solr']['port']     = '8984'
+default['solr']['pid_file'] = '/var/run/solr.pid'
+default['solr']['log_file'] = '/var/log/solr.log'
+default['solr']['install_java'] = true
