89431b3442f5511277fb7aeb0f6d552844c54d1c
John Ennew <johne@deeson.co.uk>
2015-03-11 12:41:57 +0000

    Fixes for allow override and xdebug

--- chef/cookbooks/core/vdd/recipes/xdebug.rb
+++ chef/cookbooks/core/vdd/recipes/xdebug.rb
@@ -1,3 +1,7 @@
+package "php5-xdebug" do
+  action :install
+end
+
 php_pear "xdebug" do
   action :install
 end
@@ -8,8 +12,18 @@ file "/etc/php5/mods-available/xdebug.ini" do
   only_if { File.exists?("/etc/php5/mods-available/xdebug.ini") }
 end
 
+file "/etc/php5/apache2/conf.d/xdebug.ini" do
+  action :delete
+  notifies :restart, "service[apache2]", :delayed
+  only_if { File.exists?("/etc/php5/apache2/conf.d/xdebug.ini") }
+end
+
 template "/etc/php5/mods-available/vdd_xdebug.ini" do
   source "vdd_xdebug.ini.erb"
   mode "0644"
   notifies :restart, "service[apache2]", :delayed
+end
+
+link "/etc/php5/apache2/conf.d/vdd_xdebug.ini" do
+  to "/etc/php5/mods-available/vdd_xdebug.ini"
 end
