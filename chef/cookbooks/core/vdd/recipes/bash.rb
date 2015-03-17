30bada3e5e194b42f2c4cd85701074d7967f813c
Dan James <ignitedfirestarter@gmail.com>
2015-03-17 14:44:38 +0000

    Don't show the druplicon on login.

--- chef/cookbooks/core/vdd/recipes/bash.rb
+++ chef/cookbooks/core/vdd/recipes/bash.rb
@@ -2,8 +2,3 @@ template "/home/vagrant/.bash_profile" do
   source "bash/bash_profile"
   mode "0644"
 end
-
-template "/etc/update-motd.d/11-druplicon" do
-  source "bash/11-druplicon"
-  mode "0755"
-end
