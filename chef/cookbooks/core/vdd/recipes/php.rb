cb461c4126a6fcd882c2facb81d01a6a40062acf
John Ennew <johne@deeson.co.uk>
2015-03-18 16:30:12 +0000

    added packages for sqlite3 and php support

--- chef/cookbooks/core/vdd/recipes/php.rb
+++ chef/cookbooks/core/vdd/recipes/php.rb
@@ -4,7 +4,8 @@ pkgs = [
   "php5-mysql",
   "php5-mcrypt",
   "php5-curl",
-  "php5-dev"
+  "php5-dev",
+  "php5-sqlite"
 ]
 
 pkgs.each do |pkg|
@@ -17,4 +18,23 @@ template "/etc/php5/apache2/conf.d/vdd_php.ini" do
   source "vdd_php.ini.erb"
   mode "0644"
   notifies :restart, "service[apache2]", :delayed
+end
+
+modules = [
+  "vdd_xdebug",
+  "uploadprogress",
+  "pdo_mysql",
+  "mongo",
+  "memcache",
+  "sqlite3"
+]
+
+modules.each do |mod|
+  bash "enable_php_module_#{mod}" do
+    user "root"
+    code <<-EOH
+    php5enmod #{mod}
+    EOH
+    not_if { File.exists?("/etc/php5/mods-available/#{mod}.ini") }
+  end
 end
