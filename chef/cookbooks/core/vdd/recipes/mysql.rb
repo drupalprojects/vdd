1f7e07c0c418e3820576697be291b925e937cf91
Dan James <ignitedfirestarter@gmail.com>
2015-03-18 16:23:07 +0000

    Don't install or set up the old wacky MySQL stuff.

--- chef/cookbooks/core/vdd/recipes/mysql.rb
+++ chef/cookbooks/core/vdd/recipes/mysql.rb
@@ -18,20 +18,3 @@ template "/etc/apparmor.d/tunables/alias" do
   notifies :restart, "service[apparmor]"
   only_if {node["vm"]["persist_db"]}
 end
-
-mysql_service 'default' do
-  port '3306'
-  version '5.5'
-  initial_root_password node['mysql']['server_root_password']
-  action [:create, :start]
-end
-
-mysql_config 'default' do
-  source 'vdd_my.cnf.erb'
-  notifies :restart, 'mysql_service[default]'
-  action :create
-end
-
-mysql_client 'default' do
-  action :create
-end
