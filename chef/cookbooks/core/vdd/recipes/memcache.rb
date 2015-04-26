6fb0edae5f3b5c0fbc81cea7a64df5223f6e0841
John Ennew <johne@deeson.co.uk>
2015-04-26 20:44:17 +0100

    not sure if these ini files are needed now as the php recipe covers their installation

--- chef/cookbooks/core/vdd/recipes/memcache.rb
+++ chef/cookbooks/core/vdd/recipes/memcache.rb
@@ -3,10 +3,10 @@ php_pear "memcache" do
   action :install
 end
 
-link "/etc/php5/apache2/conf.d/memcache.ini" do
-  to "/etc/php5/mods-available/memcache.ini"
-end
+#link "/etc/php5/apache2/conf.d/memcache.ini" do
+#  to "/etc/php5/mods-available/memcache.ini"
+#end
 
-link "/etc/php5/cli/conf.d/memcache.ini" do
-  to "/etc/php5/mods-available/memcache.ini"
-end
+#link "/etc/php5/cli/conf.d/memcache.ini" do
+#  to "/etc/php5/mods-available/memcache.ini"
+#end
--- chef/cookbooks/core/vdd/recipes/xdebug.rb
+++ chef/cookbooks/core/vdd/recipes/xdebug.rb
@@ -6,28 +6,28 @@ php_pear "xdebug" do
   action :install
 end
 
-file "/etc/php5/mods-available/xdebug.ini" do
-  action :delete
-  notifies :restart, "service[apache2]", :delayed
-  only_if { File.exists?("/etc/php5/mods-available/xdebug.ini") }
-end
+#file "/etc/php5/mods-available/xdebug.ini" do
+#  action :delete
+#  notifies :restart, "service[apache2]", :delayed
+#  only_if { File.exists?("/etc/php5/mods-available/xdebug.ini") }
+#end
 
-file "/etc/php5/apache2/conf.d/xdebug.ini" do
-  action :delete
-  notifies :restart, "service[apache2]", :delayed
-  only_if { File.exists?("/etc/php5/apache2/conf.d/xdebug.ini") }
-end
+#file "/etc/php5/apache2/conf.d/xdebug.ini" do
+#  action :delete
+#  notifies :restart, "service[apache2]", :delayed
+#  only_if { File.exists?("/etc/php5/apache2/conf.d/xdebug.ini") }#
+#end
 
-template "/etc/php5/mods-available/vdd_xdebug.ini" do
-  source "vdd_xdebug.ini.erb"
-  mode "0644"
-  notifies :restart, "service[apache2]", :delayed
-end
+#template "/etc/php5/mods-available/vdd_xdebug.ini" do
+#  source "vdd_xdebug.ini.erb"
+#  mode "0644"
+#  notifies :restart, "service[apache2]", :delayed
+#end
 
-link "/etc/php5/apache2/conf.d/vdd_xdebug.ini" do
-  to "/etc/php5/mods-available/vdd_xdebug.ini"
-end
+#link "/etc/php5/apache2/conf.d/vdd_xdebug.ini" do
+#  to "/etc/php5/mods-available/vdd_xdebug.ini"
+#end
 
-link "/etc/php5/cli/conf.d/vdd_xdebug.ini" do
-  to "/etc/php5/mods-available/vdd_xdebug.ini"
-end
+#link "/etc/php5/cli/conf.d/vdd_xdebug.ini" do
+#  to "/etc/php5/mods-available/vdd_xdebug.ini"
+#end
