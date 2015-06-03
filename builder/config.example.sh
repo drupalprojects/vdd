d8c02c0908112589e3981ca65beeb59137584dfe
Mike Davis <miked@deeson.co.uk>
2015-06-03 12:43:37 +0100

    Added Drupal bulider script for building a new site to configured for vdd

---
+++ builder/config.example.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Development station
+#DEV_ROOT="$HOME/projects/sites" # root directory where drupal projects are stored
+
+# User ID 1 Configuration
+USER="admin"
+PASS="admin"
+MAIL="drupal@deeson.co.uk"
