ac114ae6aea41cc85bf9198fda7b62ad154bed3b
John Ennew <johne@deeson.co.uk>
2015-08-09 23:21:01 +0100

    Added a private recipe for installing drupalextension to /opt/drupalextension

---
+++ chef/cookbooks/core/vdd/recipes/drupalextension.rb
@@ -0,0 +1,21 @@
+install_path = node["drupalextension"]["install_path"]
+
+directory install_path do
+  mode  00755
+  action :create
+  recursive true
+end
+
+template "#{install_path}/composer.json" do
+  source "drupalextension/composer.json.erb"
+  mode 0755
+end
+
+execute 'install_drupal_extension_with_composer' do
+  command 'composer install'
+  cwd install_path
+end
+
+execute 'make_behat_available_globally' do
+  command 'ln -s /opt/drupalextension/bin/behat /usr/local/bin/behat'
+end
