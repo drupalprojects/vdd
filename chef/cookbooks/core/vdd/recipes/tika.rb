7757d807f5ff96e0ebedd1ec52196143457a4432
John Ennew <johne@deeson.co.uk>
2015-08-19 22:47:21 +0200

    Fixes #6 - Added Apacke Tika to /opt/apache-tika/tika.jar and the settings for the apachesolr_attachments module

---
+++ chef/cookbooks/core/vdd/recipes/tika.rb
@@ -0,0 +1,13 @@
+directory "/opt/apache-tika" do
+  mode  00755
+  action :create
+  recursive true
+end
+
+remote_file '/opt/apache-tika/tika.jar' do
+  source 'http://mirror.arcor-online.net/www.apache.org/tika/tika-app-1.10.jar'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
