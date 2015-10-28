da0b7f837939858b4062d5991ca831675caca051
John Ennew <johne@deeson.co.uk>
2015-10-28 23:00:40 +0000

    Fixes #20 by choosing the 1.11 jar file version

--- chef/cookbooks/core/vdd/recipes/tika.rb
+++ chef/cookbooks/core/vdd/recipes/tika.rb
@@ -5,7 +5,7 @@ directory "/opt/apache-tika" do
 end
 
 remote_file '/opt/apache-tika/tika.jar' do
-  source 'http://mirror.arcor-online.net/www.apache.org/tika/tika-app-1.10.jar'
+  source 'http://mirror.arcor-online.net/www.apache.org/tika/tika-app-1.11.jar'
   owner 'root'
   group 'root'
   mode '0644'
