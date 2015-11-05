3973651e35fee35cc0b35d7024f39619d2755af3
Dan James <ignitedfirestarter@gmail.com>
2015-11-05 12:40:24 +0000

    Fix incorrect Tika file.

--- chef/cookbooks/core/vdd/recipes/tika.rb
+++ chef/cookbooks/core/vdd/recipes/tika.rb
@@ -5,7 +5,7 @@ directory "/opt/apache-tika" do
 end
 
 remote_file '/opt/apache-tika/tika.jar' do
-  source 'http://www.mirrorservice.org/sites/ftp.apache.org/tika/tika-1.11-src.zip'
+  source 'http://www.mirrorservice.org/sites/ftp.apache.org/tika/tika-app-1.11.jar'
   owner 'root'
   group 'root'
   mode '0644'
