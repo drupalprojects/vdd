313f157fe86cd30a1ee1d216115e6a8438b074c4
git <git@2967915.no-reply.drupal.org>
2017-01-25 13:39:27 -0500

    Issue #2745239 by ericjenkins, hook_awsome, LiamPower, awm: Ruby version >=2 Required

--- chef/shell/initial.sh
+++ chef/shell/initial.sh
@@ -7,10 +7,11 @@ cat "$VAGRANT_DIR/chef/shell/vdd.txt"
 
 # Upgrade Chef.
 echo "Updating Chef to 11.12.4 version. This may take a few minutes..."
+apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get update &> /dev/null
 echo "sources updated"
 echo "installing ruby and chef"
-apt-get install build-essential ruby1.9.1-dev --no-upgrade --yes
+apt-get install build-essential ruby2.2 ruby2.2-dev --no-upgrade --yes
 update-ca-certificates
 gem install chef --version="11.12.4" --no-rdoc --no-ri --conservative
 echo "installed ruby and chef"
