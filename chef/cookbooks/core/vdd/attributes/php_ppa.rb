32460089331078f3e58a46dd0511fa7c647b64e4
Ali <awm086@1059398.no-reply.drupal.org>
2018-01-31 13:17:03 -0500

    add php attributes

---
+++ chef/cookbooks/core/vdd/attributes/php_ppa.rb
@@ -0,0 +1,4 @@
+default['php7_ppa']['uri']        = 'http://ppa.launchpad.net/ondrej/php/ubuntu'
+default['php7_ppa']['key']        = 'E5267A6C'
+default['php7_ppa']['key_server'] = 'keyserver.ubuntu.com'
+
